5ccfbe70cabd508b4f6ddfda3df1370e0c058d35
RassiLevine <rassilevine@gmail.com>
2024-12-16 23:43:37 -0500

    data update 2

--- fixes/1 table update.sql
+++ fixes/1 table update.sql
@@ -1,6 +1,7 @@
 alter table Meals add MealsDesc varchar(500) not null default ''
 
-ALTER TABLE dbo.Recipe ADD IsVegan BIT NOT NULL DEFAULT 0;
+ALTER TABLE dbo.Recipe ADD IsVegan VARCHAR(3) NOT NULL DEFAULT '';
 
-ALTER TABLE dbo.Cookbook ADD SkillLevel varchar(20);
+ALTER TABLE dbo.Cookbook ADD SkillLevel varchar(20) not null DEFAULT '';
+
 select * from meals

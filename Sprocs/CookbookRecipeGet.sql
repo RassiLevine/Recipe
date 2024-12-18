7b3e4b32277d6877885625ad91d5d19112fcc831
RassiLevine <rassilevine@gmail.com>
2024-12-18 17:41:10 -0500

    cookbookrecipeget update

--- Sprocs/CookbookRecipeGet.sql
+++ Sprocs/CookbookRecipeGet.sql
@@ -8,13 +8,23 @@ as
 begin
  declare @return int = 0
 
-select cbr.CookBookRecipeId, cbr.CookbookId, r.RecipeId, cbr.Seq, r.RecipeName
+select cbr.CookBookRecipeId, cbr.CookbookId, r.RecipeId, cbr.Seq, r.RecipeName, r.isVegan, r.calories,r.RecipeStatus,
+r.CuisineId,r.DateDraft, r.DatePublished,
+     r.DateArchived, r.StaffId, 'user' = concat(s.StaffFirstName, ' ', s.StaffLastName), r.RecipePic,NumIngredients = count(ri.ingredientid)
 from CookbookRecipe cbr
 join Recipe r
 on r.RecipeId = cbr.RecipeId
+join staff s 
+on r.StaffId = s.StaffId
+join RecipeIngredient ri
+on ri.RecipeId = r.RecipeId
 where cbr.CookbookId = @CookbookId
 or @All = 1
+group by cbr.CookBookRecipeId, cbr.CookbookId, r.RecipeId, cbr.Seq, r.RecipeName, r.isVegan, r.calories,r.RecipeStatus,
+r.CuisineId,r.DateDraft, r.DatePublished,
+     r.DateArchived, r.StaffId,r.RecipePic,s.StaffFirstName, s.StaffLastName
 order by cbr.Seq
+
  return @return
 
 end
@@ -22,3 +32,5 @@ go
 
 --exec CookbookRecipeGet @cookbookid = 3
 
+
+   
